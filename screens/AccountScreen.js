9fdf355a9ebee51109e9cf844348f1e3a7b78a7e
gladyn <gladynwong@gmail.com>
2022-06-30 10:59:52 +0800

    first

---
+++ screens/AccountScreen.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { commonStyles } from "../styles/commonStyles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const API = "https://pcmob5-blog-api.gladynw.repl.co";
+const API_WHOAMI = "/whoami";
+
+export default function AccountScreen({ navigation }) {
+  const[username, setUsername] = useState("");
+
+  async function getUsername() {
+    console.log("---- Getting user name ----");
+    const token = await AsyncStorage.getItem("token");
+    console.log(`Token is ${token}`);
+    try {
+      const response = await axios.get(API + API_WHOAMI, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+      console.log("Got user name!");
+      console.log(response);
+    } catch (error) {
+      console.log("Error getting user name");
+      if (error.response) {
+        console.log(error.response.data);
+      } else {
+        console.log(error);
+      }
+    }
+  }
+
+  useEffect(() => {
+    getUsername();
+  }, []);
+
+  function signOut() {
+    AsyncStorage.removeItem("token");
+    navigation.navigate("SignIn");
+
+  }
+
+  return (
+    <View style={commonStyles.container}>
+      <Text>Account Screen</Text>
+      <Button title="Sign out" onPress={signOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
